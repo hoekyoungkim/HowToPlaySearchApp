b868d91e216f9cdf9e3e80b19dc9a08e2c58bad6
Hoekyoung Kim <hoekyouk@usc.edu>
2019-02-13 23:52:16 -0800

    changed main ui

--- app/src/main/java/com/project/tryboardgames/MainActivity.java
+++ app/src/main/java/com/project/tryboardgames/MainActivity.java
@@ -2,6 +2,8 @@ package com.project.tryboardgames;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    public void openCameraApp(View view){
+
+        // 1. open the camera app
+        Log.d("openCameraApp","button open camera app");
+//        Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//        startActivity(intent);
+        // 2. take a photo of a barcode on the boardgame box
+        // 3. process the barcode
+        // 4. find related videos on Youtube and Google search page
+
+    }
+
 }
